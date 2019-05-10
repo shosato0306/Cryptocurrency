5f00a4ef3c79b5ca0a414b94e1d89696040cf84e
sho sato <oydgw36@gmail.com>
2019-05-10 09:33:30 +0900

    Add GetBalance API.

---
+++ bitflyer/bitflyer.go
@@ -0,0 +1,107 @@
+package bitflyer
+
+import (
+	"bytes"
+	"crypto/hmac"
+	"crypto/sha256"
+	"encoding/hex"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"net/url"
+	"strconv"
+	"time"
+)
+
+const baseURL = "https://api.bitflyer.com/v1/"
+
+type APIClient struct {
+	key        string
+	secret     string
+	httpClient *http.Client
+}
+
+func New(key, secret string) *APIClient {
+	apiClient := &APIClient{key, secret, &http.Client{}}
+	return apiClient
+}
+
+func (api APIClient) header(method, endpoint string, body []byte) map[string]string {
+	timestamp := strconv.FormatInt(time.Now().Unix(), 10)
+	log.Println(timestamp)
+	message := timestamp + method + endpoint + string(body)
+
+	mac := hmac.New(sha256.New, []byte(api.secret))
+	mac.Write([]byte(message))
+	sign := hex.EncodeToString(mac.Sum(nil))
+	return map[string]string{
+		"ACCESS-KEY":       api.key,
+		"ACCESS-TIMESTAMP": timestamp,
+		"ACCESS-SIGN":      sign,
+		"Content-Type":     "application/json",
+	}
+}
+
+func (api *APIClient) doRequest(method, urlPath string, query map[string]string, data []byte) (body []byte, err error) {
+	baseURL, err := url.Parse(baseURL)
+	if err != nil {
+		return
+	}
+	apiURL, err := url.Parse(urlPath)
+	if err != nil {
+		return
+	}
+	endpoint := baseURL.ResolveReference(apiURL).String()
+	log.Printf("action=doRequest endpoint=%s", endpoint)
+	req, err := http.NewRequest(method, endpoint, bytes.NewBuffer(data))
+	if err != nil {
+		return
+	}
+	q := req.URL.Query()
+	for key, value := range query {
+		fmt.Println(key, value)
+		q.Add(key, value)
+	}
+	req.URL.RawQuery = q.Encode()
+
+	for key, value := range api.header(method, req.URL.RequestURI(), data) {
+		fmt.Println(key, value)
+		req.Header.Add(key, value)
+	}
+	resp, err := api.httpClient.Do(req)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+	body, err = ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+	fmt.Println(string(body))
+	return body, nil
+}
+
+type Balance struct {
+	CurrentCode string  `json:"currency_code"`
+	Amount      float64 `json:"amount"`
+	Available   float64 `json:"available"`
+}
+
+func (api *APIClient) GetBalance() ([]Balance, error) {
+	url := "me/getbalance"
+	resp, err := api.doRequest("GET", url, map[string]string{}, nil)
+	log.Printf("url=%s resp=%s", url, string(resp))
+	if err != nil {
+		log.Printf("action=GetBalance err=%s", err.Error())
+		return nil, err
+	}
+	var balance []Balance
+	err = json.Unmarshal(resp, &balance)
+	if err != nil {
+		log.Printf("action=GetBalance err=%s", err.Error())
+		return nil, err
+	}
+	return balance, nil
+}
--- main.go
+++ main.go
@@ -1,12 +1,14 @@
 package main
 
 import (
-	"log"
+	"fmt"
+	"trading/bitflyer"
 	"trading/config"
 	"trading/utils"
 )
 
 func main() {
 	utils.LoggingSettings(config.Config.LogFile)
-	log.Println("test")
+	apiClient := bitflyer.New(config.Config.ApiKey, config.Config.ApiSecret)
+	fmt.Println(apiClient.GetBalance())
 }
