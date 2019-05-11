36768bd9a2ffa5903cded9483f80e82fbdbfd90e
sho sato <oydgw36@gmail.com>
2019-05-11 20:28:57 +0900

    Add candle stick information API.

--- app/controllers/webserver.go
+++ app/controllers/webserver.go
@@ -3,8 +3,12 @@ package controllers
 import (
 	"cryptocurrency/app/models"
 	"cryptocurrency/config"
+	"encoding/json"
 	"fmt"
+	"log"
 	"net/http"
+	"regexp"
+	"strconv"
 	"text/template"
 )
 
@@ -22,7 +26,63 @@ func viewChartHandler(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
+type JSONError struct {
+	Error string `json:"error"`
+	Code  int    `json:"code"`
+}
+
+func APIError(w http.ResponseWriter, errMessage string, code int) {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(code)
+	jsonError, err := json.Marshal(JSONError{Error: errMessage, Code: code})
+	if err != nil {
+		log.Fatal(err)
+	}
+	w.Write(jsonError)
+}
+
+var apiValidPath = regexp.MustCompile("^/api/candle/$")
+
+func apiMakeHandler(fn func(http.ResponseWriter, *http.Request)) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		m := apiValidPath.FindStringSubmatch(r.URL.Path)
+		if len(m) == 0 {
+			APIError(w, "Not found", http.StatusNotFound)
+		}
+		fn(w, r)
+	}
+}
+
+func apiCandleHandler(w http.ResponseWriter, r *http.Request) {
+	productCode := r.URL.Query().Get("product_code")
+	if productCode == "" {
+		APIError(w, "No product_code param", http.StatusBadRequest)
+		return
+	}
+	strLimit := r.URL.Query().Get("limit")
+	limit, err := strconv.Atoi(strLimit)
+	if strLimit == "" || err != nil || limit < 0 || limit > 1000 {
+		limit = 1000
+	}
+
+	duration := r.URL.Query().Get("duration")
+	if duration == "" {
+		duration = "1m"
+	}
+	durationTime := config.Config.Durations[duration]
+
+	df, _ := models.GetAllCandle(productCode, durationTime, limit)
+
+	js, err := json.Marshal(df)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(js)
+}
+
 func StartWebServer() error {
+	http.HandleFunc("/api/candle/", apiMakeHandler(apiCandleHandler))
 	http.HandleFunc("/chart/", viewChartHandler)
 	return http.ListenAndServe(fmt.Sprintf(":%d", config.Config.Port), nil)
 }
--- app/models/candle.go
+++ app/models/candle.go
@@ -7,14 +7,14 @@ import (
 )
 
 type Candle struct {
-	ProductCode string
-	Duration    time.Duration
-	Time        time.Time
-	Open        float64
-	Close       float64
-	High        float64
-	Low         float64
-	Volume      float64
+	ProductCode string        `json:"product_code"`
+	Duration    time.Duration `json:"duration"`
+	Time        time.Time     `json:"time"`
+	Open        float64       `json:"open"`
+	Close       float64       `json:"close"`
+	High        float64       `json:"high"`
+	Low         float64       `json:"low"`
+	Volume      float64       `json:"volume"`
 }
 
 func NewCandle(ProductCode string, duration time.Duration, timeDate time.Time, open, close, high, low, volume float64) *Candle {
