37b3dc10fe4717c858224558c7dd38e2028ac52f
sho sato <oydgw36@gmail.com>
2019-05-12 10:10:30 +0900

    Add Ajax request.

--- app/controllers/webserver.go
+++ app/controllers/webserver.go
@@ -12,15 +12,15 @@ import (
 	"text/template"
 )
 
-var templates = template.Must(template.ParseFiles("app/views/google.html"))
+var templates = template.Must(template.ParseFiles("app/views/chart.html"))
 
 func viewChartHandler(w http.ResponseWriter, r *http.Request) {
-	limit := 100
-	duration := "1s"
-	durationTime := config.Config.Durations[duration]
-	df, _ := models.GetAllCandle(config.Config.ProductCode, durationTime, limit)
+	// limit := 100
+	// duration := "1s"
+	// durationTime := config.Config.Durations[duration]
+	// df, _ := models.GetAllCandle(config.Config.ProductCode, durationTime, limit)
 
-	err := templates.ExecuteTemplate(w, "google.html", df.Candles)
+	err := templates.ExecuteTemplate(w, "chart.html", nil)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 	}
