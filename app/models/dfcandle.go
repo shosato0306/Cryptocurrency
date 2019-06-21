f8ffa977abb1abc90d448baaa7ca62ec5c660a99
sho sato <oydgw36@gmail.com>
2019-06-21 22:40:28 +0900

    .

--- app/models/dfcandle.go
+++ app/models/dfcandle.go
@@ -271,8 +271,8 @@ func (df *DataFrameCandle) OptimizeEma() (performance float64, bestPeriod1 int, 
 	bestPeriod2 = 14
 
 	// for period1 := 5; period1 < 11; period1++ {
-	for period1 := 3; period1 < 7; period1++ {
-		for period2 := 7; period2 < 13; period2++ {
+	for period1 := 3; period1 < 5; period1++ {
+		for period2 := 5; period2 < 13; period2++ {
 			signalEvents := df.BackTestEma(period1, period2)
 			if signalEvents == nil {
 				continue
